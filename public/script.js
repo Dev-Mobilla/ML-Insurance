b78be68c051987256c02e4373a5329b8ac554184
dev-mobilla <>
2023-04-01 15:46:39 +0800

    GET ER

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
+window.onload = function () {
 
+    document.getElementById("er-guard-plus").addEventListener("click", setCookie);
 
-document.getElementById("er-guard-plus").addEventListener("click", setCookie);
-
+}
 function setCookie() {
     let er_guard = {
-        "TypeLogin": "INSURANCE",
         "ProductID": "INS-000002"
     }
 
     let json_string = JSON.stringify(er_guard);
 
-    document.cookie = "data=" + json_string
+    document.cookie = "MLInsurance=" + json_string
 
     console.log('cookie outside')
     console.log(document.cookie);
